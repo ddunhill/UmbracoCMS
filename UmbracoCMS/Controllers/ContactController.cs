8bc7ae8211ed3e80afd1f40f8c0f4c4fee6ce1d3
David Dunhill <taskerweb@gmail.com>
2017-12-14 13:39:16 +0000

    First Add

---
+++ UmbracoCMS/Controllers/ContactController.cs
@@ -0,0 +1,71 @@
+﻿using Umbraco.Web.Mvc;
+using System.Web.Mvc;
+using UmbracoCMS.Models;
+using log4net;
+using System.Reflection;
+//using System.Net.Mail;
+
+namespace UmbracoCMS.Controllers
+{
+    public class ContactController : SurfaceController
+    {
+        public string GetViewPath(string name)
+        {
+            return $"~/Views/Partials/Contact/{name}.cshtml";
+        }
+
+        [HttpGet]
+        public ActionResult RenderForm()
+        {
+            ContactViewModel model = new ContactViewModel();
+            return PartialView(GetViewPath("_ContactForm"), model);
+        }
+
+        [HttpPost]
+        public ActionResult RenderForm(ContactViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                //handle errors
+            }
+            return PartialView(GetViewPath("_ContactForm"), model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult SubmitForm(ContactViewModel model)
+        {
+            bool success = false;
+            if (ModelState.IsValid)
+            {
+                success = StoreMessage(model);
+            }
+            return PartialView(GetViewPath(success ? "_Success" : "_Error"));
+        }
+
+        private bool StoreMessage(ContactViewModel model)
+        {
+            ILog log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType); 
+            try
+            {
+                //Store in DB
+                return true;
+            }
+            catch (System.Exception ex)
+            {
+                log.Error("Form Error", ex);
+                return false;
+            }
+        }
+
+        //private bool SendEmail(ContactViewModel model)
+        //{
+        //    MailMessage message = new MailMessage(model.EmailAddress, "website@installumbraco.web.local");
+        //    message.Subject = string.Format("Enquiry from {0} {1} - {2}", model.FirstName, model.LastName, model.EmailAddress);
+        //    message.Body = model.Message;
+        //    SmtpClient client = new SmtpClient("127.0.0.1", 25);
+        //    client.Send(message);
+        //    return true;
+        //}
+    }
+}
---
+++ UmbracoCMS/scripts/contactForm.js
@@ -0,0 +1,16 @@
+﻿var contactForm = contactForm || {
+    init: function () {
+        this.listeners();
+    },
+    listeners: function () {
+        $(document).on('click', 'contact-submit', function (e) {
+            e.preventDefault();
+            var form = $(this).closest('form');
+            $(form).submit();
+        })
+    },
+    showResult: function () {
+        $('#form-outer').hide();
+        $('#form-result').show();
+    }
+}
